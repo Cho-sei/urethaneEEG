e57a225e46dae1960bd52f675182d9f9826d847f
Yuta Inoue <yi1306c12@gmail.com>
2019-03-24 21:44:46 +0900

    write axis_position in MatrixStim

--- kraepelin/kraepelin_components.py
+++ kraepelin/kraepelin_components.py
@@ -5,10 +5,14 @@ class MatrixStim:
     def __init__(self, win, matrix_shape, interval, center_pos, **textstim_keyargs):
 
         def axis_positions(count, interval, center):
+            base = numpy.arange((count+1) // 2)
             if count % 2 == 0: #even
-                pass
+                points = interval/2 + interval*base + center
+                return numpy.concatenate([points[::-1], points])
             else: #odd
-                pass
+                points = interval*base + center
+                return numpy.concatenate([points[::-1], points[1:]])
+
         x_axis = axis_positions(matrix_shape[1], interval[0], center_pos[0])
         y_axis = axis_positions(matrix_shape[0], interval[1], center_pos[1])
         grid_positions = numpy.meshgrid(x_axis, y_axis)
@@ -19,6 +23,7 @@ class MatrixStim:
         ]
 
     def set_matrix(self, matrix):
+        print(len(self.textstim_list), matrix.shape)
         for textstim, text in zip(self.textstim_list, numpy.nditer(matrix)):
             textstim.setText(text)
             textstim.draw()
