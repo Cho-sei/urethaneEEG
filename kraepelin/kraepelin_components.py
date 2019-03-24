f1e737e0a000ce44d16e9c6655067de2b08216ad
Yuta Inoue <yi1306c12@gmail.com>
2019-03-24 13:40:36 +0900

    write MatrixStim except for positioning

--- kraepelin/kraepelin_components.py
+++ kraepelin/kraepelin_components.py
@@ -1,34 +1,24 @@
 from psychopy import visual
 import numpy
 
-def draw_matrix(win, array2d, interval, center_pos, **textstim_keyargs):
-    """
-    draw a 2d-array as it seems.
+class MatrixStim:
+    def __init__(self, win, matrix_shape, interval, center_pos, **textstim_keyargs):
 
-    Args:
-        array2d (numpy.ndarray): 
-        interval (???): (x,y)
-        center_pos (???): (x,y)
-        textstim_keyargs (dict):
-    Returns:
-        None
-    """
+        def axis_positions(count, interval, center):
+            if count % 2 == 0: #even
+                pass
+            else: #odd
+                pass
+        x_axis = axis_positions(matrix_shape[1], interval[0], center_pos[0])
+        y_axis = axis_positions(matrix_shape[0], interval[1], center_pos[1])
+        grid_positions = numpy.meshgrid(x_axis, y_axis)
 
-    assert array2d.ndim == 2, "Matrix should be 2d."
-    #assert numpy.issubdtype(array2d.dtype, np.integer), "Matrix should be composed of integers."
+        self.textstim_list = [
+            visual.TextStim(win, pos = (x, y), **textstim_keyargs)
+            for x, y in zip(numpy.nditer(grid_positions[0]), numpy.nditer(grid_positions[1]))
+        ]
 
-    #calculate grid positions
-    def axis_positions(count, interval, center):
-        if count % 2 == 0: #even
-            pass
-        else: #odd
-            pass
-    x_axis = axis_positions(array2d.shape[1], interval[0], center_pos[0])
-    y_axis = axis_positions(array2d.shape[0], interval[1], center_pos[1])
-    grid_positions = numpy.meshgrid(x_axis, y_axis)
-
-    #draw numbers
-    for number, x, y in zip(numpy.nditer(array2d), numpy.nditer(grid_positions[0]), numpy.nditer(grid_positions[1])):
-        number_stim = visual.TextStim(win, text=str(number), pos=(x, y), **textstim_keyargs)
-        number_stim.draw()
-
+    def set_matrix(self, matrix):
+        for textstim, text in zip(self.textstim_list, numpy.nditer(matrix)):
+            textstim.setText(text)
+            textstim.draw()
