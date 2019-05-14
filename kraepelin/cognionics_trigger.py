707722ac066e16e797b470a184a8d135d0ad1caf
yi1306c12 <yi1306c12@gmail.com>
2019-05-14 17:47:22 +0900

    fix around bytes&unicode

--- kraepelin/cognionics_trigger.py
+++ kraepelin/cognionics_trigger.py
@@ -6,13 +6,14 @@ class CognionicsTrigger:
     def __init__(self, port_name, **serial_keyargs):
         self.serial_port = serial.Serial(port_name, **serial_keyargs)
 
-    def send_trigger(self, binary):
+    def send_trigger(self, char):
         """send a binary to port as a trigger.
         Another thread is used avoiding blocking main experiment.
         """
+        assert len(char) == 1, "serial.write can send only 1 byte."
         serialport_lock = threading.Lock()
         trigger_onset = threading.Thread(
-            target=self._threadsafe_trigger, args=(self.serial_port, binary, serialport_lock)
+            target=self._threadsafe_trigger, args=(self.serial_port, char.encode(), serialport_lock)
         )
         trigger_reset = threading.Timer(
             .01, self._threadsafe_trigger, args=(self.serial_port, b'\0', serialport_lock)
@@ -23,7 +24,7 @@ class CognionicsTrigger:
     @staticmethod
     def _threadsafe_trigger(serialport, binary, lock, **serial_keyargs):
         with lock:
-            written_byte = serialport.write(binary, serial_keyargs)
+            written_byte = serialport.write(binary, **serial_keyargs)
             time.sleep(.01)
         return written_byte#no meaning
 
@@ -35,4 +36,4 @@ if __name__ == "__main__":
     trigger = CognionicsTrigger(port_name)
     for char in string:
         print("order of '{}' is {}".format(char, ord(char)))
-        trigger.send_trigger(ord(char))
+        trigger.send_trigger(char)
