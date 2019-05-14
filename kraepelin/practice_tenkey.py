3fc73e76696bc8dc8230473d1b298acc1a5e09fa
Yuta Inoue <yi1306c12@gmail.com>
2019-05-15 02:04:29 +0900

    rewrite using KraepelinWindow

--- kraepelin/practice_tenkey.py
+++ kraepelin/practice_tenkey.py
@@ -1,105 +1,85 @@
-from psychopy import visual, sound, event, core
-import random
-import sys
 import collections
+import random
 
-#set global escape
-event.globalKeys.add(key='escape', func=sys.exit)
+from psychopy import visual, sound, event
 
-KEY_LIST = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+def practice_tenkey(kraepelin_window, sounds):
+    inst_stim = visual.TextStim(kraepelin_window, height=80)
+    ans_text = visual.TextStim(kraepelin_window, height=80, pos=(0,-100))
 
-redo_flag = True
+    kraepelin_window.display_stimuli(
+        [visual.TextStim(win, text=u'テンキー入力練習', height=80)],
+        sound=sounds.into_ten,
+    )
+    kraepelin_window.display_stimuli(
+        [visual.TextStim(win, 'Wait...', height=80)],
+        sound=sounds.start_ten,
+    )
+    kraepelin_window.display_stimuli(
+        [visual.TextStim(win, 'Wait...', height=80)],
+        sound=sounds.start_demo,
+    )
 
-win = visual.Window(units='pix', fullscr=True, allowGUI=False)
+    redo_flag = True
+    while redo_flag:
+        #reset parameter
+        stim_list = sum([random.sample(kraepelin_window.KEY_LIST, k=len(kraepelin_window.KEY_LIST)) for _ in range(2)], [])
+        false_counter = 0
 
-inst_stim = visual.TextStim(win, height=80)
-ans_text = visual.TextStim(win, height=80, pos=(0,-100))
+        kraepelin_window.display_stimuli(
+            [visual.TextStim(win, 'Start!', height=80)],
+            wait_time=2.,
+        )
+        for number in stim_list:
+            inst_stim.setText(number)
 
-opening_msg = visual.TextStim(win, text=u'テンキー入力練習', height=80)
-wait_msg = visual.TextStim(win, 'Wait...', height=80)
-start_msg = visual.TextStim(win, 'Start!', height=80)
-finish_msg = visual.TextStim(win, 'Finish!', height=80)
-redo_msg = visual.TextStim(win, 'Redo', height=80)
+            while True:
+                kraepelin_window.display_stimuli(
+                    [inst_stim],
+                    wait_time=0.,
+                )
+                keys = event.waitKeys(keyList=kraepelin_window.KEY_LIST)
+                answer_number = kraepelin_window.KEY_LIST.index(keys[0])
+                ans_text.setText(answer_number)
 
-correct_msg = visual.TextStim(win, '○', height=80, pos=(0,-200))
-false_msg = visual.TextStim(win, '×', height=80, pos=(0,-200))
+                if answer_number == int(number):
+                    kraepelin_window.display_stimuli(
+                        [ans_text, inst_stim, visual.TextStim(kraepelin_window, '○', height=80, pos=(0,-200))],
+                        wait_time=1.,
+                    )
+                    break
+                else:
+                    #try the same key
+                    kraepelin_window.display_stimuli(
+                        [ans_text, inst_stim, visual.TextStim(kraepelin_window, '×', height=80, pos=(0,-200))],
+                        wait_time=1.,
+                    )
+                    false_counter += 1
 
-stim_list = random.sample(KEY_LIST, len(KEY_LIST)) + random.sample(KEY_LIST, len(KEY_LIST))
+        if false_counter < 4:
+            redo_flag = False
+        else:
+            kraepelin_window.display_stimuli(
+                [visual.TextStim(win, 'Redo', height=80)],
+                sound=sounds.redo_ten,
+            )
 
+    kraepelin_window.display_stimuli(
+        [visual.TextStim(win, 'Finish! Press Enter', height=80)],
+        sound=sounds.finish_demo,
+    )
+    event.waitKeys(keyList=['num_enter'])
 
-SoundNamedTuple = collections.namedtuple('SoundNamedTuple', [
-	'into_ten', 'start_ten', 'redo_ten', 'start_demo', 'finish_demo'])
-sound_namedtuple = SoundNamedTuple(**{soundname:sound.Sound('sounds/'+soundname+'.wav') for soundname in SoundNamedTuple._fields})
+if __name__ == "__main__":
+    import sys
+    #set global escape
+    event.globalKeys.add(key='escape', func=sys.exit)
 
+    SoundNamedTuple = collections.namedtuple('SoundNamedTuple', [
+        'into_ten', 'start_ten', 'redo_ten', 'start_demo', 'finish_demo'])
+    sound_namedtuple = SoundNamedTuple(**{soundname:sound.Sound('sounds/'+soundname+'.wav') for soundname in SoundNamedTuple._fields})
 
-#--start------------------------------------------------------------------------------
-opening_msg.draw()
-win.flip()
-sound_namedtuple.into_ten.play()
-core.wait(sound_namedtuple.into_ten.duration)
+    from kraepelin_stimuli import KraepelinWindow
+    win = KraepelinWindow(units='pix', fullscr=True, allowGUI=False)
 
-wait_msg.draw()
-win.flip()
-sound_namedtuple.start_ten.play()
-core.wait(sound_namedtuple.start_ten.duration)
-
-sound_namedtuple.start_demo.play()
-core.wait(sound_namedtuple.start_demo.duration)
-
-while redo_flag:
-
-    start_msg.draw()
-    win.flip()
-
-    core.wait(2)
-
-    false_counter = 0
-
-    for count in stim_list:
-        inst_stim.setText(count)
-        
-        inst_stim.draw()
-        win.flip()
-
-        while True:
-            keys = event.waitKeys(keyList=KEY_LIST)
-            
-            answer_number = KEY_LIST.index(keys[0])
-
-            ans_text.setText(answer_number)
-            ans_text.draw()
-            inst_stim.draw()
-            win.flip()
-
-            if answer_number == int(count):
-                ans_text.draw()
-                inst_stim.draw()
-                correct_msg.draw()
-                win.flip()
-                core.wait(1)
-                break
-            else:
-                ans_text.draw()
-                inst_stim.draw()
-                false_msg.draw()
-                win.flip()
-                core.wait(1)
-                inst_stim.draw()
-                win.flip()
-                false_counter += 1
-
-    if false_counter < 4:
-        redo_flag = False
-    else:
-        redo_msg.draw()
-        win.flip()
-        sound_namedtuple.redo_ten.play()
-        core.wait(sound_namedtuple.redo_ten.duration)
-
-finish_msg.draw()
-win.flip()
-
-sound_namedtuple.finish_demo.play()
-core.wait(sound_namedtuple.finish_demo.duration)
-
-event.waitKeys(keyList=['space'])
+    practice_tenkey(win, sound_namedtuple)
