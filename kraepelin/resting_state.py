c9027ee1a1c507fa169805f9ce7d023ed6f635fd
yi1306c12 <yi1306c12@gmail.com>
2019-05-15 14:26:45 +0900

    omit sounds from arg

--- kraepelin/resting_state.py
+++ kraepelin/resting_state.py
@@ -1,6 +1,9 @@
 import random
 from psychopy import visual, sound, core, gui
 from kraepelin_stimuli import get_fixation_stim
+SoundNamedTuple = collections.namedtuple('SoundNamedTuple', [
+	'into_EOresting', 'into_ECresting', 'into_subtract', 'finish_resting', 'answer_of_subtraction'])
+sound_namedtuple = SoundNamedTuple(**{soundname:sound.Sound('sounds/'+soundname+'.wav') for soundname in SoundNamedTuple._fields})
 
 #start-----------------------------------------------------------------
 def start_waitmsg(func):
@@ -24,44 +27,44 @@ def restingstate_recording(win, wait_time):
 	win.flip()
 
 @start_waitmsg
-def eyesopen_restingstate_recording(win, sounds):
-	sounds.into_EOresting.play()
-	core.wait(sounds.into_EOresting.duration)
+def eyesopen_restingstate_recording(win):
+	sound_namedtuple.into_EOresting.play()
+	core.wait(sound_namedtuple.into_EOresting.duration)
 
 	restingstate_recording(win, 60)
 
-	sounds.finish_resting.play()
-	core.wait(sounds.finish_resting.duration)
+	sound_namedtuple.finish_resting.play()
+	core.wait(sound_namedtuple.finish_resting.duration)
 
 @start_waitmsg
-def eyesclose_restingstate_recording(win, sounds):
+def eyesclose_restingstate_recording(win):
 	beep = sound.Sound(value=1000, secs=1.0)
 	beep.setVolume(0.5)
 
-	sounds.into_ECresting.play()
-	core.wait(sounds.into_ECresting.duration)
+	sound_namedtuple.into_ECresting.play()
+	core.wait(sound_namedtuple.into_ECresting.duration)
 	beep.play()
 
 	restingstate_recording(win, 60)
 
-	sounds.finish_resting.play()
-	core.wait(sounds.finish_resting.duration)
+	sound_namedtuple.finish_resting.play()
+	core.wait(sound_namedtuple.finish_resting.duration)
 
 @start_waitmsg
-def subtractingstate_recording(win, sounds):
+def subtractingstate_recording(win):
 	beep = sound.Sound(value=1000, secs=1.0)
 	beep.setVolume(0.5)
 	dlg = gui.Dlg(title=u'回答')
 	dlg.addField(u'答え:','')
 
-	sounds.into_subtract.play()
-	core.wait(sounds.into_subtract.duration)
+	sound_namedtuple.into_subtract.play()
+	core.wait(sound_namedtuple.into_subtract.duration)
 	beep.play()
 
 	restingstate_recording(win, 60)
 
-	sounds.finish_resting.play()
-	core.wait(sounds.finish_resting.duration)
+	sound_namedtuple.finish_resting.play()
+	core.wait(sound_namedtuple.finish_resting.duration)
 
 	win.setMouseVisible(True)
 	win.flip()
@@ -77,10 +80,7 @@ if __name__ == "__main__":
 	event.globalKeys.add(key='escape', func=sys.exit)
 
 	win = KraepelinWindow(units='pix', fullscr=True, allowGUI=False)
-	SoundNamedTuple = collections.namedtuple('SoundNamedTuple', [
-		'into_EOresting', 'into_ECresting', 'into_subtract', 'finish_resting', 'answer_of_subtraction'])
-	sound_namedtuple = SoundNamedTuple(**{soundname:sound.Sound('sounds/'+soundname+'.wav') for soundname in SoundNamedTuple._fields})
 
-	eyesopen_restingstate_recording(win, sound_namedtuple)
-	eyesclose_restingstate_recording(win, sound_namedtuple)
-	print(subtractingstate_recording(win, sound_namedtuple))
+	eyesopen_restingstate_recording(win)
+	eyesclose_restingstate_recording(win)
+	print(subtractingstate_recording(win))
