1493874c2fb02738cd2180849ac22874b15e1bed
Cho-sei <knct0420@gmail.com>
2019-05-15 18:08:04 +0900

    add img

--- kraepelin/test.py
+++ kraepelin/test.py
@@ -2,20 +2,9 @@ from psychopy import visual, core, event, gui
 
 win = visual.Window(size=(1920, 1080), units='pix', fullscr=True, allowGUI=False)
 
-output_num = visual.TextStim(win, height=80, bold=True)
+FVAS = visual.ImageStim(win, image='imgs/inst_FVAS.png', units='pix')
 
-dlg = gui.Dlg(title=u'回答')
-dlg.addField('answer:','')
-
-core.wait(3)
-
-win.setMouseVisible(True)
+FVAS.draw()
 win.flip()
 
-dlg.show()
-
-output_num.setText(dlg.data[0])
-output_num.draw()
-win.flip()
-
-core.wait(3)
+event.waitKeys(keyList=['space'])
