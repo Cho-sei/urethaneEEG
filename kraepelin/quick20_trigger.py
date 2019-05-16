06fcd21111ae0a83568ec0091a202d4a6881cded
yi1306c12 <yi1306c12@gmail.com>
2019-05-17 04:36:29 +0900

    set serial port automatically?

--- kraepelin/quick20_trigger.py
+++ kraepelin/quick20_trigger.py
@@ -2,7 +2,12 @@ from cognionics_trigger import CognionicsTrigger
 
 port_name = 'COM7'#get automatically
 
-trigger = CognionicsTrigger(port_name, baudrate=57600)
+import serial.tools.list_ports
+for portinfo in serial.tools.list_ports.comp:orts():
+    if portinfo.serial_number == 'A1368SV7A':#TODO:register serials for other quick-20
+        trigger = CognionicsTrigger(port_name, baudrate=57600)
+else:
+    raise RuntimeError("not find quick-20 (serial_number:A1368SV7A)")
 send_trigger = trigger.send_trigger
 
 
