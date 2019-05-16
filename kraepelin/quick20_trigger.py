271e8098e44eb61d6052355fd81479fa00584422
yi1306c12 <yi1306c12@gmail.com>
2019-05-17 05:00:28 +0900

    complete

--- kraepelin/quick20_trigger.py
+++ kraepelin/quick20_trigger.py
@@ -1,19 +1,21 @@
 from cognionics_trigger import CognionicsTrigger
 
-port_name = 'COM7'#get automatically
-
 import serial.tools.list_ports
-for portinfo in serial.tools.list_ports.comp:orts():
+for portinfo in serial.tools.list_ports.comports():
     if portinfo.serial_number == 'A1368SV7A':#TODO:register serials for other quick-20
-        trigger = CognionicsTrigger(port_name, baudrate=57600)
+        trigger = CognionicsTrigger(portinfo.device, baudrate=57600)
+        print(portinfo.serial_number, portinfo.device)
+        break
 else:
     raise RuntimeError("not find quick-20 (serial_number:A1368SV7A)")
 send_trigger = trigger.send_trigger
 
 
 if __name__ == "__main__":
+    print(trigger.serial_port.name, trigger.serial_port.baudrate)
     import sys
     import time
-    for c in sys.argv[1]:
-        send_trigger(c)
-        time.sleep(1.)
+    char = chr(int(sys.argv[1]))
+    send_trigger(char)
+    print(char)
+    time.sleep(1.)
