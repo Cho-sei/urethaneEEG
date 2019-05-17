95f3395cd8b9b73c80c07851353c9245c39add9b
yi1306c12 <yi1306c12@gmail.com>
2019-05-17 16:45:12 +0900

    underconstruction

---
+++ kraepelin/dialog_alternative.py
@@ -0,0 +1,35 @@
+from psychopy import core, visual, event
+
+def dialog_alternative(kraepelin_window):
+    display_text = visual.TextStim(win, pos=(0, 0), bold=True)
+    display_text.setText("")
+    cursor_rect = visual.Rect(win, width=20, height=80)
+    while True:
+        #get key & append | delete
+        keys = event.getKeys(keyList=kraepelin_window.NUMKEY_NAME + kraepelin_window.ENTER_NAME)
+        for key in keys:
+            if key in kraepelin_window.NUMKEY_NAME:
+                display_text.text += str(kraepelin_window.NUMKEY_NAME.index(key))
+            elif key in kraepelin_window.ENTER_NAME:
+                return int(display_text)
+            elif key in kraepelin_window.DELETE_NAME:
+                display_text.text.pop()
+        #set cursor
+        t = core.getTime()
+
+
+        kraepelin_window.display_stimuli(
+            [display_text],
+            wait_time=0.1,
+        )
+
+
+if __name__ == "__main__":
+    import sys
+    event.globalKeys.add(key='escape', func=sys.exit)
+
+    from kraepelin_stimuli import KraepelinWindow
+    win = KraepelinWindow(fullscr=True, units='pix')
+    win.setMouseVisible(False)
+
+    print(dialog_alternative(win))
